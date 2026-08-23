7527c49e04d162cbb6346f43e630d17433ed3dc3
Anis <anis.zahaf2746@email.com>
2026-08-23 16:12:17 +0100

    self-contain express app inside api/index.ts to fix vercel module resolution

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig, Plugin } from 'vite';
-import { app } from './server';
+import { app } from './api/index';
 
 function expressPlugin(): Plugin {
   return {
